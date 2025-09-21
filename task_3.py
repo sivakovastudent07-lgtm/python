a10b607c2175ed5b01b29caebe35253e05f403bb
sivakovastudent07-lgtm <sivakova.student.07@gmail.com>
2025-09-21 22:27:46 +0300

    Delete task_3.py

--- task_3.py
+++
@@ -1,7 +0,0 @@
-a = input("Введите пароль: ")
-if len(a) < 16:
-    print('Слишком короткий')
-elif a.isalpha() or a.isdigit:
-    print('Слабый пароль')
-else:
-    print('Надёжный пароль')        
