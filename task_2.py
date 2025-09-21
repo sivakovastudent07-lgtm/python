02fcdc298272716291954513f96dd446e8c81377
sivakovastudent07-lgtm <sivakova.student.07@gmail.com>
2025-09-21 21:56:26 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,3 @@
+str_in = input("Введите строку на английском: ")
+result = str_in.replace('a','').replace('e','').replace('i','').replace('o','').replace('u', '')
+print(result) 
---
+++ task_8.py
@@ -0,0 +1,5 @@
+a= input("Введите слово: ")
+if a == a[::-1]:
+    print ('palindrom')
+else:
+    print('ne palindrom')
