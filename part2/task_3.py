70b0cbb47569b8828833b5305e5b9332c249f2f3
sivakovastudent07-lgtm <sivakova.student.07@gmail.com>
2025-09-29 00:09:48 +0300

    done

---
+++ part2/task_3.py
@@ -0,0 +1,19 @@
+import time
+
+def log_calls(filename):
+    def decorator(func):
+        def wrapper(a, b):  # фиксированные аргументы
+            current_time = time.ctime()
+            func_name = func.__name__
+
+            args_text = f"{a}, {b}"
+            result = func(a, b)
+
+            log_line = f"[{current_time}] Функция '{func_name}' вызвана с аргументами: ({args_text}), результат: {result}"
+
+            with open(filename, "a", encoding="utf-8") as f:
+                f.write(log_line)
+
+            return result
+        return decorator
+    return decorator
