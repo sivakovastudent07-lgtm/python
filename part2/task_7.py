ccffa5be7439aef7c5dac2983a2dcac122224b9f
sivakovastudent07-lgtm <sivakova.student.07@gmail.com>
2025-09-29 00:19:08 +0300

    done

---
+++ part2/task_7.py
@@ -0,0 +1,20 @@
+def merge_list(l1, l2):
+    merged = []  # новый список для результата
+    i, j = 0, 0  # индексы для l1 и l2
+
+    while i < len(l1) and j < len(l2):
+        if l1[i] < l2[j]:
+            merged.append(l1[i])
+            i += 1
+        else:
+            merged.append(l2[j])
+            j += 1
+
+    while i < len(l1):
+        merged.append(l1[i])
+        i += 1
+    while j < len(l2):
+        merged.append(l2[j])
+        j += 1
+
+    return merged
