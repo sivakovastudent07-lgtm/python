e52c652e343109a842745656da89e5b8d7ce05a5
sivakovastudent07-lgtm <sivakova.student.07@gmail.com>
2025-09-29 00:02:42 +0300

    при одинаковых ключах значение перезаписывается на значение из второго словаря

---
+++ part2/task_2.py
@@ -0,0 +1,9 @@
+def merge_dicts(dict1, dict2):
+    for key in dict2:
+        if key in dict1:
+            if type(dict1[key]) == dict and type(dict2[key]) == dict:
+                merge_dicts(dict1[key], dict2[key])
+            else:
+                dict1[key] = dict2[key] 
+        else:
+            dict1[key] = dict2[key]  
