6b5b6cf6c8c1f94ecb71445a6178102d22ba5db9
sivakovastudent07-lgtm <sivakova.student.07@gmail.com>
2025-09-29 00:20:41 +0300

    done

---
+++ part2/task_8.py
@@ -0,0 +1,16 @@
+import time
+
+def timing(func):
+    """
+    Декоратор для измерения времени выполнения функции в миллисекундах
+    для функции с фиксированными аргументами a, b, c.
+    """
+    def timed_f(a=0, b=0, c=0):
+        start_time = time.time()  
+        result = func(a, b, c)   
+        end_time = time.time()    
+        elapsed_ms = (end_time - start_time) * 10**(-3)  # перевод в миллисекунды
+        print("Время выполнения функции '{}': {} мс".format(func.__name__, elapsed_ms))
+        return result
+
+    return timed_f
