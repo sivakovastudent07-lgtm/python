78d69c205d966106956c0de7235863c29c36b2a0
sivakovastudent07-lgtm <sivakova.student.07@gmail.com>
2025-09-29 19:23:59 +0300

    для заглавных глассных

--- task_2.py
+++ task_2.py
@@ -1,3 +1,4 @@
 str_in = input("Введите строку на английском: ")
-result = str_in.replace('a','').replace('e','').replace('i','').replace('o','').replace('u', '')
+result = str_in.replace('a','').replace('e','').replace('i','').replace('o','').replace('u', '').replace('A','').replace('O','').replace('E','').replace('U','').replace('I','')
+
 print(result) 
