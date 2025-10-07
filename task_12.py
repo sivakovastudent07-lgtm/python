8f83d5dd1638dffae8d46a42ef61101667a10b8a
sivakovastudent07-lgtm <sivakova.student.07@gmail.com>
2025-10-07 15:00:55 +0300

    через ф-строки

--- task_12.py
+++ task_12.py
@@ -18,12 +18,12 @@ subtotal = base + extra_minutes + extra_sms + extra_data
 tax = subtotal * 0.02
 total = subtotal + tax
 
-print("Базовая стоимость тарифа: " + str(round(base, 2)) + " руб.")
+print(f"Базовая стоимость тарифа: {str(round(base, 2))} руб.")
 if extra_minutes > 0:
-    print("Доп. минуты: " + str(round(extra_minutes, 2)) + " руб.")
+    print(f"Доп. минуты: {str(round(extra_minutes, 2))} руб.")
 if extra_sms > 0:
-    print("Доп. SMS: " + str(round(extra_sms, 2)) + " руб.")
+    print(f"Доп. SMS: {str(round(extra_sms, 2))} руб.")
 if extra_data > 0:
-    print("Доп. интернет: " + str(round(extra_data, 2)) + " руб.")
-print("Налог: " + str(round(tax, 2)) + " руб.")
-print("Итого к оплате: " + str(round(total, 2)) + " руб.")
+    print(f"Доп. интернет: {str(round(extra_data, 2))} руб.")
+print(f"Налог: {str(round(tax, 2))} руб.")
+print(f"Итого к оплате: {str(round(total, 2))} руб.")
