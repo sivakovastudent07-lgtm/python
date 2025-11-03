2c1375467cc32253cce0a5ac9fcbc3f678fe7a74
sivakovastudent07-lgtm <sivakova.student.07@gmail.com>
2025-11-03 22:06:37 +0300

    змейка теперь не выглядит раздробленной

--- task3_part1.py
+++ task3_part1.py
@@ -4,21 +4,21 @@ fig, ax = plt.subplots(figsize=(8, 2))
 
 #ТЕЛО
 for i in range(10):
-    ax.add_patch(plt.Circle((i * 0.6, 0), 0.25, color='green'))
+    ax.add_patch(plt.Circle((i * 0.4, 0), 0.25, color='green'))
 
 # ГОЛОВА
-ax.add_patch(plt.Circle((10 * 0.6, 0), 0.3, color='green'))
+ax.add_patch(plt.Circle((10 * 0.4, 0), 0.3, color='green'))
 
 # ГЛАЗА
-ax.add_patch(plt.Circle((10 * 0.6 + 0.1, 0.08), 0.05, color='white'))
-ax.add_patch(plt.Circle((10 * 0.6 + 0.1, -0.08), 0.05, color='white'))
-ax.add_patch(plt.Circle((10 * 0.6 + 0.12, 0.08), 0.02, color='black'))
-ax.add_patch(plt.Circle((10 * 0.6 + 0.12, -0.08), 0.02, color='black'))
+ax.add_patch(plt.Circle((10 * 0.4 + 0.1, 0.08), 0.05, color='white'))
+ax.add_patch(plt.Circle((10 * 0.4 + 0.1, -0.08), 0.05, color='white'))
+ax.add_patch(plt.Circle((10 * 0.4 + 0.12, 0.08), 0.02, color='black'))
+ax.add_patch(plt.Circle((10 * 0.4 + 0.12, -0.08), 0.02, color='black'))
 
 # ЯЗЫК
-ax.plot([10 * 0.6 + 0.3, 10 * 0.6 + 0.45], [0, 0], color='red', linewidth=2)
-ax.plot([10 * 0.6 + 0.45, 10 * 0.6 + 0.5], [0, 0.05], color='red', linewidth=2)
-ax.plot([10 * 0.6 + 0.45, 10 * 0.6 + 0.5], [0, -0.05], color='red', linewidth=2)
+ax.plot([10 * 0.4 + 0.3, 10 * 0.4 + 0.45], [0, 0], color='red', linewidth=2)
+ax.plot([10 * 0.4 + 0.45, 10 * 0.4 + 0.5], [0, 0.05], color='red', linewidth=2)
+ax.plot([10 * 0.4 + 0.45, 10 * 0.4 + 0.5], [0, -0.05], color='red', linewidth=2)
 
 ax.set_aspect('equal')
 ax.axis('off')
